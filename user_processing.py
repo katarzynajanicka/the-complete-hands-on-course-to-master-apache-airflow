7b034d93e2174fb4806b5bc4241a4c0ae3f73064
Kate <70230905+katarzynajanicka@users.noreply.github.com>
2021-09-07 12:10:41 +0200

    Add files via upload
    Coding your first data pipeline with Airflow

---
+++ user_processing.py
@@ -0,0 +1,34 @@
+from airflow.models import DAG
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.http.sensors.http import HttpSensor
+
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020, 1, 1) 
+}
+
+with DAG('user_processing', schedule_interval='@daily', 
+        default_args=default_args,
+        catchup=False) as dag:
+    # Define tasks/operators
+    creating_table = SqliteOperator(
+        task_id='creating_table',
+        sqlite_conn_id='db_sqlite',
+        sql='''
+        CREATE TABLE users(
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            country TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL PRIMARY KEY
+        );
+        '''
+    )
+
+    is_api_available = HttpSensor(
+        task_id='is_api_available',
+        http_conn_id='user_api',
+        endpoint='api/'
+    )
