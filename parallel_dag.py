07df8f03369c569ce560e5d72b6b1d3ad15ff7ee
Kate <70230905+katarzynajanicka@users.noreply.github.com>
2021-09-08 10:27:31 +0200

    Add parallel_dag.py

---
+++ parallel_dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020, 1, 1)
+}
+
+with DAG('parallel_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
+    
+    task_1 = BashOperator(
+        task_id='task_1',
+        bash_command='sleep 3'
+    )
+
+    task_2 = BashOperator(
+        task_id='task_2',
+        bash_command='sleep 3'
+    )
+    task_3 = BashOperator(
+        task_id='task_3',
+        bash_command='sleep 3'
+    )
+    task_4 = BashOperator(
+        task_id='task_4',
+        bash_command='sleep 3'
+    )
+
+    task_1 >> [task_2, task_3] >> task_4
